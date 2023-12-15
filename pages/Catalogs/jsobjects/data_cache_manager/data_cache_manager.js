97f41c36df98aa968416175a275cd87b74e133a0
Steven Liu <hujia.liuhj@gmail.com>
2023-12-15 02:50:38 +0000

    Added logs for cache_manager

--- pages/Catalogs/jsobjects/data_cache_manager/data_cache_manager.js
+++ pages/Catalogs/jsobjects/data_cache_manager/data_cache_manager.js
@@ -1,4 +1,5 @@
 export default {
+	__LOG_TAG__: "[data_cache_manager]",
 	__cache_expiry_ms: 30 * 1000,
 	load_cache () {
 		data_all_catalogs_hash.catalog_hash = appsmith.store.data_all_catalogs_catalog_hash;
@@ -12,6 +13,7 @@ export default {
 		storeValue("data_cache_manager_last_saved", Date.now());
 	},
 	is_cache_valid() {
+		console.log(this.__LOG_TAG__, Date.now(), appsmith.store.data_cache_manager_last_saved, Date.now() - appsmith.store.data_cache_manager_last_saved);
 		return appsmith.store.data_cache_manager_last_saved && 
 			(Date.now() - appsmith.store.data_cache_manager_last_saved) < this.__cache_expiry_ms;
 	}
