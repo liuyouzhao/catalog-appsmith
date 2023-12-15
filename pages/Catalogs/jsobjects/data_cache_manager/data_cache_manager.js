520f8945916682b62c654bb091e03a31e84491b2
Steven Liu <hujia.liuhj@gmail.com>
2023-12-15 02:47:24 +0000

    Fixed cache validation.

--- pages/Catalogs/jsobjects/data_cache_manager/data_cache_manager.js
+++ pages/Catalogs/jsobjects/data_cache_manager/data_cache_manager.js
@@ -1,4 +1,5 @@
 export default {
+	__cache_expiry_ms: 30 * 1000,
 	load_cache () {
 		data_all_catalogs_hash.catalog_hash = appsmith.store.data_all_catalogs_catalog_hash;
 		data_all_catalogs_tree.catalogs_menu_tree_roots = appsmith.store.data_all_catalogs_tree_catalogs_menu_tree_roots;
@@ -8,14 +9,10 @@ export default {
 		storeValue("data_all_catalogs_catalog_hash", data_all_catalogs_hash.catalog_hash);
 		storeValue("data_catalog_details_details_list", data_catalog_details.details_list);
 		storeValue("data_all_catalogs_tree_catalogs_menu_tree_roots", data_all_catalogs_tree.catalogs_menu_tree_roots);
-
-		storeValue("page_catalog__cache_valid", true);
-
-		setTimeout(() => {
-			storeValue("page_catalog__cache_valid", false);
-		}, 60 * 1000);
+		storeValue("data_cache_manager_last_saved", Date.now());
 	},
 	is_cache_valid() {
-		return appsmith.store.page_catalog__cache_valid;
+		return appsmith.store.data_cache_manager_last_saved && 
+			(Date.now() - appsmith.store.data_cache_manager_last_saved) < this.__cache_expiry_ms;
 	}
 }
